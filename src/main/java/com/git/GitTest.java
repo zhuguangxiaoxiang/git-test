bcf1df6fe9ae3a754cdbdaa754a22331ea937c83
Tom <Tom@gmail.com>
2022-02-05 21:02:35 +0800

    second commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -11,5 +11,6 @@ package com.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }

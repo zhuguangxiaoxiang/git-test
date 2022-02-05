6e1fc8b6fc8c10b1555f9705a25322686d302288
zhuguangxiaoxiang <93524739+zhuguangxiaoxiang@users.noreply.github.com>
2022-02-05 22:38:12 +0800

    github update

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -18,6 +18,6 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("push test!");
         System.out.println("pull test!");
-        System.out.println("gitee test!");
+        System.out.println("github项目更新...");
     }
 }

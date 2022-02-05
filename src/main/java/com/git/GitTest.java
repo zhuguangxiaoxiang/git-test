5a68543fc6714fefac0d3a0b83b8a4e37a895c81
Tom <Tom@gmail.com>
2022-02-05 20:59:21 +0800

    first commit

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.git;
+
+/**
+ * ClassName: GitTest
+ * Description:
+ * date: 2022/2/5 20:55
+ *
+ * @author Administrator
+ * @since JDK 1.8
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
